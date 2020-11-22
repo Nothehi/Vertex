d6c887fb0af7bcb2f89842a46ab5155b766e3fb0
nothehi <nodehi7813@gmail.com>
2020-11-22 22:04:48 +0330

    Add rtl

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +8,16 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant, e }) {
+      addVariant('rtl', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.rtl .${e(`rtl${separator}${className}`)}`
+        }, 
+        {
+          variants: ['responsive'],
+        })
+      })
+    }),
+  ],
 }
